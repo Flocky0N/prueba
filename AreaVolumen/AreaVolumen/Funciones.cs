e71b2f5ef1f586ba2b5d4fcc01aae0ba7c469212
Naty <artigabaibelin@gmail.com>
2024-10-21 21:45:57 -0600

    Add files via upload
    Primera prueba

---
+++ AreaVolumen/AreaVolumen/Funciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaVolumen
+{
+    class Funciones
+    {
+        public static double ACubo(double LogCubo)
+        {
+            double AreCubo;
+            AreCubo = 6 * (Math.Pow(LogCubo, 2));
+            return AreCubo;
+        }
+           public static double Vcubo(double LogCubo)
+           {
+           double VolumCubo;
+           VolumCubo=Math.Pow(LogCubo,3);
+            return VolumCubo;
+           }
+        public static double AEsfera(double radio)
+        {
+            double AreEsfera;
+            AreEsfera = 4 * (Math.PI) * (Math.Pow(radio, 2));
+            return AreEsfera;
+        }
+        public static double VEsfera(double radio)
+        {
+            double VolumEsfera;
+            VolumEsfera = 4/3 * (Math.PI) * (Math.Pow(radio, 3));
+            return VolumEsfera;
+        }
+    }
+}
---
+++ AreaVolumen/AreaVolumen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace AreaVolumen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double AreaCubo, LongitudCubo, VolumenCubo, AreaEsfera, RadioEsfera, VolumenEsfera, VolumenTotal;
+            Console.WriteLine("Ingrese la Longitud del cubo");
+            LongitudCubo = double.Parse(Console.ReadLine());
+
+            AreaCubo = Funciones.ACubo(LongitudCubo);
+            Console.WriteLine("El area del cubo es: "+ AreaCubo);
+
+            VolumenCubo = Funciones.Vcubo(LongitudCubo);
+            Console.WriteLine("El volumen del cubo es: " + VolumenCubo);
+
+            Console.WriteLine("Ingrese el radio de la esfera");
+            RadioEsfera = double.Parse(Console.ReadLine());
+            AreaEsfera = Funciones.AEsfera(RadioEsfera);
+            Console.WriteLine("El área de la esfera es: " + AreaEsfera);
+
+            VolumenEsfera = Funciones.VEsfera(RadioEsfera);
+            Console.WriteLine("El volumen de la esfera es: " + VolumenEsfera);
+            VolumenTotal = VolumenEsfera + VolumenCubo;
+            Console.WriteLine("El volumen total es: " + VolumenTotal);
+
+            Console.WriteLine("Presion cualquier tecla para salir..");
+            Console.ReadLine();
+        }
+    }
+}
