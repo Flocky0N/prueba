288462f5ad067f7e1b7d1545009ee82890adf477
Diego-0-0 <do702234@gmail.com>
2024-10-22 14:52:08 -0600

    Update Program.cs
    Agregando una nueva funcionalidad

--- AreaVolumen/AreaVolumen/Program.cs
+++ AreaVolumen/AreaVolumen/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+//Agregando nuevo funcionamiento
+using System.Threading;
 
 namespace AreaVolumen
 {
@@ -28,6 +29,39 @@ namespace AreaVolumen
             Console.WriteLine("El volumen de la esfera es: " + VolumenEsfera);
             VolumenTotal = VolumenEsfera + VolumenCubo;
             Console.WriteLine("El volumen total es: " + VolumenTotal);
+
+
+            //Agregando funcion extra
+             double CalculoMental;
+
+            Console.WriteLine("En que numero estás pensando? ");
+            CalculoMental = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Calculando...");
+            Thread.Sleep(2000);
+
+            Console.Clear();
+            Console.WriteLine("Recolectando información...");
+            Thread.Sleep(2000);
+
+            Console.Clear();
+            Console.WriteLine("Creando conexiones neuronales...");
+            Thread.Sleep(2000);
+
+            Console.Clear();
+            Console.WriteLine("Renderizando resultado...");
+            Thread.Sleep(2000);
+
+            Console.Clear();
+            Console.WriteLine("Este proceso puede tardar... Por favor espere...");
+            Thread.Sleep(4000);
+
+            Console.Clear();
+            Console.WriteLine("Estas pensando en el numero: "+CalculoMental);
+            Console.ReadKey();
+
+            
 
             Console.WriteLine("Presion cualquier tecla para salir..");
             Console.ReadLine();
